b1b09f53cf623bd7e3eab70ec7f2ce2698ee2bda
Jon Kirkwood <re_kirky@hotmail.com>
2024-05-02 17:08:02 +1000

    Update __init.py

--- custom_components/advantage_air_myair3/__init.py
+++ custom_components/advantage_air_myair3/__init.py
@@ -1,13 +1,37 @@
 """The integration for Advantage Air MyAir3 system."""
 import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+
 DOMAIN = "advantage_air_myair3"
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup(hass, config):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Advantage Air MyAir3 from a config entry."""
+    # Store the configuration entry in hass.data to make it accessible to the platform
+    hass.data[DOMAIN] = {
+        "ip_address": entry.data["ip_address"]
+    }
+
+    # Set an example state, this can be changed or removed as necessary
     hass.states.set("advantage_air_myair3.integration", "Ready")
 
-    _LOGGER.info("Advantage Air MyAir 3 Ready!")
+    _LOGGER.info("Advantage Air MyAir3 Ready!")
+
+    # Forward the setup to the sensor platform
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
 
     # Return boolean to indicate that initialization was successful.
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    # Remove the config entry from hass.data
+    hass.data.pop(DOMAIN)
+    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+
+    # Return boolean to indicate the unload was successful
     return True

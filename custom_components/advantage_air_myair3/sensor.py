5801e6c230c947de3c64171ac25c7a70aa71937b
Jon Kirkwood <re_kirky@hotmail.com>
2024-05-02 16:13:19 +1000

    Update sensor.py

--- custom_components/advantage_air_myair3/sensor.py
+++ custom_components/advantage_air_myair3/sensor.py
@@ -77,10 +77,9 @@ async def async_setup_platform(
     discovery_info: DiscoveryInfoType | None = None
 ) -> None:
     # Set up the sensor platform asynchronously
-    add_entities(PowerSensor())
     sensors = [ZonePowerSensor(i) for i in range(1, 2)]
-    add_entities(sensors)
-  
+    async_add_entities(PowerSensor(),sensors)
+      
 async def async_get_zone_name(hass, zone):
     global url
     site = f"{url}getZoneData?zone={zone}"

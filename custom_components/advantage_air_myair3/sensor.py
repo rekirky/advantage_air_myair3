ac2afc797749289263acce6656cc13c906b1274c
Jon Kirkwood <re_kirky@hotmail.com>
2024-05-02 16:24:20 +1000

    Update sensor.py

--- custom_components/advantage_air_myair3/sensor.py
+++ custom_components/advantage_air_myair3/sensor.py
@@ -74,7 +74,7 @@ async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
     async_add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
+    discovery_info: DiscoveryInfoType | None = None,
     add_entities: AddEntitiesCallback
     ) -> None:
     # Set up the sensor platform asynchronously
